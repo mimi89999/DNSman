fc20057e9f8e012ecfb3170b1fc5f2be2374d3d7
otakuchiyan <codeotakuchiyan@gmail.com>
2016-02-01 21:47:50 +0800

    Fix restored list when list cleared

--- app/src/main/java/io/github/otakuchiyan/dnsman/DNSListActivity.java
+++ app/src/main/java/io/github/otakuchiyan/dnsman/DNSListActivity.java
@@ -25,6 +25,7 @@ public class DNSListActivity extends ListActivity {
     private SharedPreferences sp;
     private SharedPreferences.Editor sped;
     private ArrayList<String> dnsList;
+    private ListView mListView;
     private ArrayAdapter<String> adapter;
 
     private String[] default_list = {
@@ -40,22 +41,27 @@ public class DNSListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
 
         sp = PreferenceManager.getDefaultSharedPreferences(this);
+        sped = sp.edit();
+        mListView = getListView();
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setTitle(R.string.pref_dns_list);
         dnsList = new ArrayList<>(sp.getStringSet("dnslist", new HashSet<String>()));
-        if(dnsList.size() == 0){
+        if(sp.getBoolean("dnslist_firstenter", false)){
             dnsList.addAll(Arrays.asList(default_list));
+            sped.putBoolean("dnslist_firstenter", true);
+            sped.apply();
         }
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dnsList);
         setListAdapter(adapter);
 
-        final ListView listView = getListView();
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
             @Override
             public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-
+                int count = mListView.getCheckedItemCount();
+                mode.setTitle(String.format("%d", count));
             }
 
             @Override
@@ -74,7 +80,7 @@ public class DNSListActivity extends ListActivity {
             public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.delete:
-                        SparseBooleanArray selectedItems = listView.getCheckedItemPositions();
+                        SparseBooleanArray selectedItems = mListView.getCheckedItemPositions();
                         ArrayList<String> deletingString = new ArrayList<>();
                         //Get will be deleted entry
                         for(int i = 0; i != selectedItems.size(); i++){
