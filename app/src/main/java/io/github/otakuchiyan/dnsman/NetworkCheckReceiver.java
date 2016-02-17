af4229f8e1bde8c985f11dd4251460d6efeb3b43
otakuchiyan <codeotakuchiyan@gmail.com>
2016-02-17 20:12:56 +0800

    Remove useless code, enabled minify, updated version code

--- app/src/main/java/io/github/otakuchiyan/dnsman/NetworkCheckReceiver.java
+++ app/src/main/java/io/github/otakuchiyan/dnsman/NetworkCheckReceiver.java
@@ -3,23 +3,15 @@ package io.github.otakuchiyan.dnsman;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
-import android.os.Build;
-import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.text.TextUtils;
 import android.widget.Toast;
 import android.util.Log;
 
 public class NetworkCheckReceiver extends BroadcastReceiver {
     static boolean isFirstConnect = true;
-    private SharedPreferences sp;
 
     public void onReceive(Context context, Intent intent) {
         ConnectivityManager cm;
